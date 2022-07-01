d626886fdd1d8cdcf502cd5fd74379ad8ba5adb7
김성진 <zzrkdl@naver.com>
2022-07-02 02:08:16 +0900

    2022.07.02 commit

--- 02MyBasicControl/ViewController.swift
+++ 02MyBasicControl/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn2: UIButton!
     @IBOutlet weak var btn3: UIButton!
     @IBOutlet var buttons: [UIButton]!  //[] 배열임을 알 수 있음
+    @IBOutlet weak var tfInput: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     
     //화면이 로딩완료시 자동호출 되는 함수
     override func viewDidLoad() {
@@ -25,8 +27,15 @@ class ViewController: UIViewController {
         labelTitle.text = "타이틀입니다."
         labelTitle.textColor = UIColor.orange
         
-       // btn1.backgroundColor = UIColor.red
+        btn1.backgroundColor = UIColor.red
         
+        for btn in buttons {
+            btn.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+            //alpha : 0.0 ~ 1.0
+            //RGB : 0.0 ~ 1.0
+            btn.backgroundColor = UIColor(red: 255/255, green: 100/255, blue: 100/255, alpha: 1)
+        }
+        buttons[0].backgroundColor = UIColor.blue
     }
 
     //액션함수가 들어가는 곳
@@ -34,15 +43,19 @@ class ViewController: UIViewController {
         //로그를 남기는 법
         print("btn1 클릭됨")
         labelTitle.text = "btn1 클릭됨"
+        imageView.image = UIImage(named:"image1")
+    }
+    @IBAction func onBtnInput(_ sender: UIButton) {
+        let titleText = tfInput.text
+        labelTitle.text = titleText
     }
     @IBAction func onBtn2Click(_ sender: UIButton) {
-        print("btn2 클릭됨")
-        labelTitle.text = "btn2 클릭됨"
+        imageView.image = UIImage(named:"image2")
     }
     
-    @IBAction func onBtn3Click(_ sender: UIButton) {
-        print("btn3 클릭됨")
-        labelTitle.text = "btn3 클릭됨"
-    }
+    //화면캡쳐 단축키
+    //CMD + SHIFT + 4
+    // 부분 캡쳐
+    //CMD + SHIFT + 2
 }
 
