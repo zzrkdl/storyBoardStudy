2bdf51e8daf1456743f9b3d603b8a4f6cd7dca96
김성진 <zzrkdl@naver.com>
2022-07-01 16:52:48 +0900

    Initial Commit

---
+++ 01FirstApp/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  01FirstApp
+//
+//  Created by 김성진 on 2022/07/01.
+//
+
+// 프로젝트의 시작점 소스 파일
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+        //앱이 맨처음 구동되었을때, 실행되는 함수
+        //초기화 코드들...
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ 01FirstApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+
+// 스토리보드 UI에 대응하기 위한 컨트롤소스
+// 한화면 - 스토리보드 화면 1개 - 뷰컨트롤러 소스 1개
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //뷰가 로딩되었을 때 자동호출되는 함수.
+        //초기화 코드들
+        
+    }
+
+
+}
+
