ce3a8249ac04c78e186392461d427cc5d933b669
김성진 <zzrkdl@naver.com>
2022-07-04 23:57:37 +0900

    2022.07.04 Review

---
+++ NewEx01.playground/Contents.swift
@@ -0,0 +1,22 @@
+//IOS UI 기본 라이브러리 포함
+import UIKit
+
+//var : 변수
+//greeting : 변수이름
+//= : (대입문 오른쪽 값을 왼쪽 변수에 넣는다)
+//"Hello, playground" : 쌍따옴표 문자열 데이타
+//세미콜론이 선택적, 넣어도 되고 안 넣어도 됨.
+var greeting = "Hello, playground"
+
+//기본 출력문
+print(greeting)
+print("Swift Programming")
+//print('swift programming') //단따옴표는 지원 안함
+
+//주석문(Comment) : 개발자가 정보를 수록하고 실행에는 영향을 주지 않음
+//한줄 주석문 //
+/*
+ 여러줄
+ 주석문
+ 입니다
+ */
