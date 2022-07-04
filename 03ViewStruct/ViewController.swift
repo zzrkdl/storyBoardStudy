29889f7e11a699dc3ed147cba7c27c86fbab9bc6
김성진 <zzrkdl@naver.com>
2022-07-04 22:18:07 +0900

    2022.07.04 navigater

--- 03ViewStruct/ViewController.swift
+++ 03ViewStruct/ViewController.swift
@@ -9,15 +9,64 @@
 //     (코드로 이동(Present, Dismiss), Segue로 이동)
 //     수직적인 이동 : 간단히 떴다가 사라지는 화면
 
+//네비게이션 컨트롤러 뷰 관리 방법
+//1. 다음 화면으로 이동하기
+// self.navigationController?.pushViewController
+//2. 이전 화면으로 이동하기
+// popViewController
+//3. 맨 앞으로 이동하기 root뷰로
+// popToRootViewController
+//4. 특정번째 뷰로 이동하기(2번째, 3번째)
+// popToViewController(newVC, animated:true)
+//5. 특정뷰를 삭제할때
+// navigationController?.viewControllers 배열에서 삭제. remove(at:)
+//6. 특정뷰를 중간에 추가할때
+// navigationController?.viewControllers 배열에서 추가. insert(at:)
+
+//뷰 컨트롤러의 수명주기 (Life Cycle)
+//viewDidLoad : 뷰가 생성될때 한번만 호출됨.(초기화 용도)
+//viewWillAppear : 뷰가 화면에 보일때마다 호출됨. (화면 갱신 용도)
+//viewDidAppear : 뷰가 화면에 보이고나서 매번 호출됨
+//viewWillDisappear : 뷰가 화면에 사라지기 전마다 호출됨.(데이타 전달)
+//viewDidDisappear : 뷰가 화면에 사라지고나서 매번 호출됨.
+//viewWillLayoutSubviews : 컨트롤(서브뷰)들의 레이아웃을 잡기 전에 호출됨.
+//viewDidLayoutSubviews : 컨트롤들의 레이아웃을 잡고 나서 호출됨.
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //Outlet 들어가는 곳
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        print("viewDidLoad")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("viewWillLayoutSubviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("viewDidLayoutSubviews")
+    }
+    
+    
     //Action 함수 들어가는 곳
     
     //네비게이션 컨트롤러에서 코드로 이동하기
@@ -26,7 +75,12 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(newVC, animated: true)
     }
     
-
-
+    @IBAction func onBtnPopup(_ sender: UIButton) {
+        let newVC = self.storyboard?.instantiateViewController(withIdentifier: "PopupVC") as! PopupViewController
+        self.present(newVC, animated: true, completion: nil)
+        
+        
+    }
+    
+    
 }
-
