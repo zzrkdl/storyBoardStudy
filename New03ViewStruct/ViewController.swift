9d9aa62b454a98c26bb8352e0812652368eb5dbe
김성진 <zzrkdl@naver.com>
2022-07-13 00:27:06 +0900

    Initial Commit

---
+++ New03ViewStruct/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+
+//  화면을 이동하는 방법 2가지
+//  1. 네비게이션 컨트롤러 이용(navigation Controller) - 권장! , 수평이동
+//     (코드로 이동(Push, Pop), Segue로 이동)
+//     수평적인 이동 : 카카오택시 앱 -> 예약: 자기정보 -> 콜정보 -> 옵션1 -> 옵션2 -> 옵션3 ...
+//  2. 팝업 창으로 이동 : 간단한 이벤트 화면(로그인, 회원가입) , 수직이동
+//     (코드로 이동(Present, Dismiss), Segue로 이동)
+//     수직적인 이동 : 간단히 떴다가 사라지는 화면
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
