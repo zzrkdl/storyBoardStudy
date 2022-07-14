8541a383975ec5d14007b3d697f1304f5a58d034
김성진 <zzrkdl@naver.com>
2022-07-14 23:27:16 +0900

    2022.07.14 Login Join Agree End

--- NewEx04_LoginJoin/AgreeViewController.swift
+++ NewEx04_LoginJoin/AgreeViewController.swift
@@ -6,10 +6,22 @@ import UIKit
 
 class AgreeViewController: UIViewController {
 
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            appDelegate.isAgree = true
+        }else {
+            appDelegate.isAgree = false
+        }
+    }
     
-
+    @IBAction func onBtnDone(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
