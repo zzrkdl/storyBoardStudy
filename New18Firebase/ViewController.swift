91aae0d87b4ac074cb904b36e300f0e65ffcf1b5
김성진 <zzrkdl@naver.com>
2022-07-25 23:41:45 +0900

    2022.07.25 userLogout , userJoin

--- New18Firebase/ViewController.swift
+++ New18Firebase/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     func updateUI(){
         if let user = Auth.auth().currentUser {
             //로그인 된 상태(세션지원- 자동로그인 유지 지원)
-            labelLogin.text = "로그인상태 : \(user.email!)"
+            labelLogin.text = "로그인상태 : \(user.email!)로 로그인됨"
         }else {
             //로그아웃 된 상태
             labelLogin.text = "로그인상태 : 로그아웃"
@@ -47,6 +47,39 @@ class ViewController: UIViewController {
             }
         }else {
             print("로그인 계정 또는 암호를 확인해주세요.")
+        }
+    }
+    
+    //회원가입
+    @IBAction func onBtnLogout(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
+        do{
+            try firebaseAuth.signOut()
+        }catch let signOutError as NSError {
+            print("SignOutError : %@", signOutError)
+        }
+        updateUI()
+    }
+    @IBAction func onBtnJoin(_ sender: UIButton) {
+        if let textID = textfieldID.text, let textPW = textfieldPW.text {
+            if textID.count < 1 || textPW.count < 1 {
+                print("아이디나 암호가 짧습니다.")
+                return
+            }
+            Auth.auth().createUser(withEmail: textID, password: textPW){
+                //후행클로저
+                authResult , error in
+                guard let user = authResult?.user, error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                    
+                }
+                //회원가입 성공
+                print("\(String(describing: user.email))회원가입 성공함")
+                print("\(user.uid) 회원가입 성공함")
+            }
+        } else {
+            print("아이디나 암호가 없습니다.")
         }
     }
 }
